873f461c4eb40f25fb22637e6d3faeb562afabb8
narendraojha8 <58454568+narendraojha8@users.noreply.github.com>
2025-07-02 15:30:25 +0530

    adding update and delete functionality

--- src/controllers/createusercontroller.js
+++ src/controllers/createusercontroller.js
@@ -1,5 +1,6 @@
 import createHttpError from "node-http-error";
 import { UserModel } from "../model/UserModel.js";
+import mongoose from "mongoose";
 import { validateUser } from "../services/createUserService.js";
 
 export const createUserController = async(req,res,next)=>{
@@ -24,25 +25,73 @@ export const createUserController = async(req,res,next)=>{
   }
 }
 
-export const getUserController = async(req,res,next)=>{
+export const getUserController = async (req, res, next) => {
   try {
-    res.send("getUserController")
-  } catch (error) {
-    next(error)
-  }
-}
+    const userId = req.params.id;
+    console.log(mongoose.Types.ObjectId.isValid(userId));
+    let userExists;
+    if(userId){
+      if(mongoose.Types.ObjectId.isValid(userId)){
+        userExists = await UserModel.findById(userId);
+        if(userExists){
+          res.status(200).json({ user: userExists });
+        }else{
+          throw createHttpError(404, "User with this email does not exist");
+        }
+      }else{
+        throw createHttpError(400, "Invalid userid provided");
+      }
 
-export const UpdateUserController = async(req,res,next)=>{
-  try {
-    res.send("UpdateUserController")
+    }else{
+      let users = await UserModel.find({});
+      res.status(200).json({ users: users });
+    }
+    
   } catch (error) {
-    next(error)
+    console.log(error);
+    next(error);
   }
-}
+};
+
+export const UpdateUserController = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    console.log(mongoose.Types.ObjectId.isValid(userId));
+    // const isValidUser = validateUser(req?.body);
+    if (userId) {
+      if (mongoose.Types.ObjectId.isValid(userId)) {
+        const user = await UserModel.findByIdAndUpdate(userId, req.body, {
+          new: true, // return the updated document
+          runValidators: true, // run schema validations
+          overwrite: false, // set to true if you want to replace entire doc
+        });
+        if (user) {
+          return res.status(200).json({ user: user });
+        } else {
+          throw createHttpError(404, "User with this email does not exist");
+        }
+      } else {
+        throw createHttpError(400, "Invalid userid provided");
+      }
+    } else {
+      throw createHttpError(400, "Userid not provided to update");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const DeleteUserController = async(req,res,next)=>{
   try {
-    res.send("DeleteUserController")
+    const userId = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(userId)){
+       throw createHttpError(400, "Invalid userid provided");
+    }
+    const deletedUser = await UserModel.findByIdAndDelete(userId);
+    if(!deletedUser){
+      throw createHttpError(404, "User with this email does not exist");
+    }
+      return res.status(200).json({ user: deletedUser });
   } catch (error) {
     next(error)
   }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,9 @@ import express from 'express';
 const router = express.Router();
 import { createUserController,UpdateUserController,DeleteUserController,getUserController } from '../controllers/createusercontroller.js';
 router.route("").get(getUserController);
+router.route("/:id").get(getUserController);
+
 router.route("").post(createUserController);
-router.route("").put(UpdateUserController);
-router.route("").delete(DeleteUserController);
+router.route("/:id").put(UpdateUserController);
+router.route("/:id").delete(DeleteUserController);
 export default router;
